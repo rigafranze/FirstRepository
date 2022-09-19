4bc9f400b8740566fddf422845b0b1f1e6663f14
rigafranze <rigaufa20@gmail.com>
2022-09-19 13:52:51 +0200

    Hajsuebung_PrimeSieve_2

--- Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/EratosthenesPrimeSieve.java
+++ Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/EratosthenesPrimeSieve.java
@@ -1,5 +1,22 @@
 package erdpresserl190180.htlgkr.Hausuebung_PrimeSieve;
 
-public abstract class EratosthenesPrimeSieve implements PrimeSieve{
-    
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    int obergrenze;
+
+    public EratosthenesPrimeSieve(int i) {
+        obergrenze = i;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        if(p<2){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
 }
