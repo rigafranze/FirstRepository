d7f86c64d742d854c69d06b4e4b502f609226fb1
rigafranze <rigaufa20@gmail.com>
2022-09-26 08:51:07 +0200

    last udate 26.09

--- Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/EratosthenesPrimeSieve.java
+++ Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/EratosthenesPrimeSieve.java
@@ -1,22 +1,45 @@
 package erdpresserl190180.htlgkr.Hausuebung_PrimeSieve;
 
 public class EratosthenesPrimeSieve implements PrimeSieve{
-    int obergrenze;
-
+    private int count;
+    private boolean[] numbers;
     public EratosthenesPrimeSieve(int i) {
-        obergrenze = i;
+        count = i;
+        numbers = new boolean[count + 1];
+        numbers[0] = true;
+        numbers[1] = true;
     }
 
     @Override
     public boolean isPrime(int p) {
-        if(p<2){
-            return false;
+
+        //Durchlauf so lang, wie groß die Obergrenze ist (count bzw. numbers) - 0, 1 wurden geskipped (wegen Komplikationen - 0/0)
+        for (int currentNumber = 2; currentNumber < numbers.length; currentNumber++) {
+            //current<Number + 1, weil die eigene Zahl nicht kontrolliert werden muss. (bsp. ob 2 ein vielfaches von 2 ist)
+            for (int numberToCheck = currentNumber + 1; numberToCheck < numbers.length; numberToCheck++) {
+                //ist "number" ein Vielfaches von currentNumber?
+                //Alle Prime Nummmern sind FALSE
+                if(numberToCheck % currentNumber == 0){
+                    numbers[numberToCheck] = true;
+                }
+            }
         }
-        return true;
+
+        return false;
     }
 
     @Override
     public void printPrimes() {
+        isPrime(0);
 
+        for (int i = 2; i < numbers.length; i++) {
+            if(numbers[i] == false){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public boolean[] getNumbers() {
+        return numbers;
     }
 }
--- Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/Main.java
+++ Hausuebung_PrimeSieve/src/erdpresserl190180/htlgkr/Hausuebung_PrimeSieve/Main.java
@@ -2,6 +2,7 @@ package erdpresserl190180.htlgkr.Hausuebung_PrimeSieve;
 
 public class Main {
     public static void main(String[] args) {
-
+        EratosthenesPrimeSieve p1 = new EratosthenesPrimeSieve(1000);
+        p1.printPrimes();
     }
 }
